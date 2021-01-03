5e1f7beb7b7d908140b080e1de8da5e379695563
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-03 23:31:45 +0530

    added django http json view for products
    configured pylint with djando in vscode/settings.json

--- app_online_store/products/urls.py
+++ app_online_store/products/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from products.views import (
-    ProductListView,
-    ProductDetailView,
+    # ProductListView,
+    # ProductDetailView,
+    product_list,
+    product_detail,
     ManufacturerListView,
     ManufacturerDetailView,
 )
 
 urlpatterns = [
-    path("products", ProductListView.as_view(), name="product_list"),
-    path("product/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
-    path("manus", ManufacturerDetailView.as_view(), name="manu_list"),
-    path("manu/<int:pk>", ManufacturerDetailView.as_view(), name="manu_detail"),
+    path("products", product_list, name="product_list"),
+    path("products/<int:pk>", product_detail, name="product_detail"),
+    path("manus", ManufacturerListView.as_view(), name="manu_list"),
+    path("manus/<int:pk>", ManufacturerDetailView.as_view(), name="manu_detail"),
 ]
--- app_online_store/products/views.py
+++ app_online_store/products/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
@@ -14,11 +16,41 @@ class ManufacturerDetailView(DetailView):
     template_name = "detail.html"
 
 
-class ProductListView(ListView):
-    model = Product
-    template_name = "list.html"
+def product_list(request):
+    products = Product.objects.all()
+    data = {"products": list(products.values())}
+    response = JsonResponse(data)
+    return response
 
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "detail.html"
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        response = JsonResponse(
+            {
+                "products": {
+                    "name": product.name,
+                    "manu": product.manufacturer.name,
+                    "addedAt": product.added_at,
+                }
+            }
+        )
+
+    except Product.DoesNotExist:
+        response = JsonResponse(
+            {
+                "error": "could not find that product",
+            },
+            status=404,
+        )
+    return response
+
+
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = "list.html"
+
+
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = "detail.html"
