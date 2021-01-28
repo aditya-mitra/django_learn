275082efcc4c690727105458e85678e038e2bcc6
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-28 19:03:12 +0530

    account registration link created

--- app_mini_socio/client/requester.py
+++ app_mini_socio/client/requester.py
@@ -12,11 +12,27 @@ def request1():
 
     print("the responded data is", response.json())
 
-def request2():
-    token = 'Token e70d61107e43de571ce870ddf9a59f849b17144e'
-    headers = {'Authorization':token}
 
-    response = requests.get(BASE_URL+'/profiles/',headers=headers)
+def request2():
+    token = "Token e70d61107e43de571ce870ddf9a59f849b17144e"
+    headers = {"Authorization": token}
+
+    response = requests.get(BASE_URL + "/profiles/", headers=headers)
+
+    print("status code", response.status_code)
+
+    print("the responded data is", response.json())
+
+
+def request3():
+    data = {
+        "username": "all_auth_user",
+        "email": "one@users.com",
+        "password1": "a long password # 23",
+        "password2": "a long password # 23",  # confirm password
+    }
+
+    response = requests.post(BASE_URL + "/rest-auth/registration", data=data)
 
     print("status code", response.status_code)
 
@@ -24,4 +40,6 @@ def request2():
 
 
 if __name__ == "__main__":
-    request2()
+    # request1()
+    # request2()
+    request3()
