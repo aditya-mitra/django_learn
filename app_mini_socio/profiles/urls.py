f386c9b0c95e6d2f4f06eb474620881ab768baa8
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-01-28 19:37:29 +0530

    correct dictionary and url_patterns

--- app_mini_socio/profiles/urls.py
+++ app_mini_socio/profiles/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from profiles.views import ProfileListViewset
 
-profile_list = ProfileListViewset.as_view({"get", "list"})
-profile_detail = ProfileListViewset.as_view({"get", "retrieve"})
+profile_list = ProfileListViewset.as_view({"get": "list"})
+profile_detail = ProfileListViewset.as_view({"get": "retrieve"})
 
-urlpatterns = [path("profiles/", profile_list, name="profile_list")]
-urlpatterns = [path("profiles/<int:pk>/", profile_detail, name="profile_detail")]
+urlpatterns = [
+    path("profiles/", profile_list, name="profile_list"),
+    path("profiles/<int:pk>/", profile_detail, name="profile_detail"),
+]
